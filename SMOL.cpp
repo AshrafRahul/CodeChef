d5330a9a2f25ef88d5eec30b8e2e5433f87864ee
AshrafRahul <m.ashraful.islam007@gmail.com>
2022-11-08 00:14:23 +0600

    Coding

--- SMOL.cpp
+++ SMOL.cpp
@@ -1,1 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    long long int n,k;
+    int a;
+    cin>>a;
+    while(a--){
+        cin>>n>>k;
+        if(k==0) cout<<n<<endl;
+        else cout<<n%k<<endl;
+    }
 
+}
