3a6e4fbab805e0ad62622170383b04002d52e1a3
Md Ashraful Islam <m.ashraful.islam007@gmail.com>
2024-12-09 23:20:34 +0600

    solved

---
+++ MAX_DIFF.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin >> t;
+    while(t--) {
+        int a,b;
+        cin >> a >> b;
+
+        cout << abs(a-abs(a-b)) << '\n';
+    }
+
+    return 0;
+}
